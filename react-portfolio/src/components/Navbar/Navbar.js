417b87a08cc80f76276994d3edcd54b5bab94567
suziestephen <suzietstephen@gmail.com>
2021-04-29 20:44:22 +1000

    removing pages links in navbar

--- react-portfolio/src/components/Navbar/Navbar.js
+++ react-portfolio/src/components/Navbar/Navbar.js
@@ -1,38 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./navbar.css";
 
 //Navbar
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg">
+      <a href="/" className="navbar" />
         My Portfolio
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About Me
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/projects"
-              className={window.location.pathname === "/projects" ? "nav-link active" : "nav-link"}
-            >
-              My Projects
-            </Link>
-          </li>
-        </ul>
-      </div>
+      
+
+
     </nav>
   );
 }
